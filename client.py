7c8ee7cdf2cf57af3b4e0ff056f5500275887bca
Parth-444 <parthkannu0@gmail.com>
2026-02-17 14:39:40 +0530

    add file_loader_node that reads on your important files

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ from langchain.tools import tool
 from langchain_ollama import ChatOllama
 from typing import TypedDict, List
 from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import asyncio
 import json
 import yaml
@@ -11,20 +12,8 @@ with open("prompts/file_selector.yaml") as f:
     p = yaml.safe_load(f)
 
 llm = ChatOllama(model="llama3:8B")
-# response = llm.invoke("what is the full name of hitler, and why was he able to rise in power despite being from a poor family")
-
-# print(response)
 
 async def get_mcp_tools():
-    # client = MultiServerMCPClient(
-    #     {
-    #         "repo_tools": {
-    #             "command": "uv",
-    #             "args": ["main.py"],
-    #             "transport": "stdio"
-    #         }
-    #     }
-    # )
     client = MultiServerMCPClient(
     {
         "github_server": {
@@ -44,17 +33,21 @@ async def get_mcp_tools():
     return tools
 
 tools = asyncio.run(get_mcp_tools())
+
 # llm_with_tools = llm.bind_tools(tools)
 repo_tree_tool = next(t for t in tools if t.name == "get_repo_tree")
 list_repos_tool = next(t for t in tools if t.name == "list_repos")
-
+get_file_content_tool = next(t for t in tools if t.name == "get_file_content")
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap = 150)
 
 
 class AgentState(TypedDict):
+
     messages: str
     repo_name : str | None
     repo_tree: List
     important_files : List
+    chunks: List
 
 async def planner_node(state: AgentState) -> AgentState:
     """Takes the repo name then gets the repo tree from MCP server"""
@@ -105,17 +98,64 @@ async def structure_analyzer_node(state: AgentState) -> AgentState:
         raw = (await llm.ainvoke(fix_prompt)).content
         parsed = json.loads(raw)
 
-    state["important_files"] = parsed
+    state["important_files"] = parsed["important_files"]
     return state
     
+
+
+
+async def file_loader_node(state: AgentState) -> AgentState:
+    """Takes path for important files and loads them into chunks"""
+
+    all_chunks = []
+    MAX_CHARS = 40_00
+
+    for file_path in state["important_files"]:
+
+        # file_content = (await get_file_content_tool.ainvoke({"repo_name": state["repo_name"], "path": file_path}))[0]
+        raw = (await get_file_content_tool.ainvoke({"repo_name": state["repo_name"], "path": file_path}))[0]
+        file_content = json.loads(raw["text"])
+
+        # print("RAW FILE CONTENT:", file_content)
+
+
+        # if not file_content["content"]:
+        #     continue
+        if not file_content.get("content"):
+            continue
+
+        content = file_content["content"]
+
+        if len(content) > MAX_CHARS:
+            content = content[:25000]  + "..." + content[:-10000] 
+            pieces = text_splitter.split_text(content)
+        else:
+
+            pieces = text_splitter.split_text(file_content["content"])
+
+        for i, piece in enumerate(pieces):
+            all_chunks.append({
+                "path": file_path,
+                "chunk_id": i,
+                "content": piece
+            })
+
+    state["chunks"] = all_chunks
+    return state
+
+
+
+
 
 workflow = StateGraph(AgentState)
 
 workflow.add_node("planner_node", planner_node)
 workflow.add_node("structure_analyzer_node", structure_analyzer_node)
+workflow.add_node("file_loader_node", file_loader_node)
 
 workflow.add_edge(START, "planner_node")
 workflow.add_edge("planner_node", 'structure_analyzer_node')
+workflow.add_edge("structure_analyzer_node", "file_loader_node")
 
 
 
@@ -125,8 +165,9 @@ initial_state = {
     "messages": "",
     "repo_name": "emotion-detection",
     "repo_tree": [],
-    "important_files": []
+    "important_files": [],
+    "chunks": []
 }
 
 result = asyncio.run(app.ainvoke(initial_state))
-print("important files ", result["important_files"])
+print("Chunks: ", result["chunks"])
